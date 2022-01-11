d7063f94152ededa7fcb579a46368a699c9f0c74
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-12 03:14:02 +0530

    main

--- onepizza.py
+++ onepizza.py
@@ -11,12 +11,14 @@ all_dislikes=[]
 
 for pr in preflst:
     if pr['likes'][0]!='0':
-        for like in pr['likes'][1:-1]:
+        x=int(pr['likes'][0])+1
+        for like in pr['likes'][1:x]:
             if like not in all_likes:
                 all_likes.append(like)
     
     if pr['dislikes'][0]!='0':
-        for dislike in pr['dislikes'][1:-1]:
+        x=int(pr['dislikes'][0])+1
+        for dislike in pr['dislikes'][1:x]:
             if dislike not in all_dislikes:
                 all_dislikes.append(dislike)
 
