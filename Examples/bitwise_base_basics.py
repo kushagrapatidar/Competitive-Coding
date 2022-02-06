05c298ab932432598eb75b91adc1d86816742a1b
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-06 22:16:16 +0530

    bitwise

--- Examples/bitwise_base_basics.py
+++ Examples/bitwise_base_basics.py
@@ -12,6 +12,40 @@ for in_put in inputs:
     K = in_put[1]
     mid = len(in_put[2])//2
     string = in_put[2]
+    length=len(string)
+    oprations = 0
+    for i in range(mid+1):
+        if string[i]!=string[-i]:
+            oprations+=1
+    if oprations>K:
+        print("NO")
+
+    elif K==oprations:
+        print("YES") 
+               
+    elif (K-oprations)%2==0:
+        print("YES")
+
+    else:
+        print("NO")    
+
+####################################################################
+
+T  = int(input())
+
+inputs = []
+for ind in range(T):
+    
+    N, K = map(int, input().split())
+    
+    binary_string = input()
+    inputs.append([N, K, binary_string])
+
+print(inputs)
+for in_put in inputs:
+    K = in_put[1]
+    mid = len(in_put[2])//2
+    string = in_put[2]
     oprations = 0
     i=0
 
@@ -30,4 +64,4 @@ for in_put in inputs:
         print("YES")
 
     else:
-        print("NO")    
+        print("NO") 
