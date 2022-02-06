e8533cadeffbc46ad0cab27ad72c7374ffa1020d
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-06 23:24:15 +0530

    main

--- Examples/Digit_Mult_K.py
+++ Examples/Digit_Mult_K.py
@@ -4,17 +4,19 @@ for _ in range(int(input())):
     if S=='0':
         print(0)
     else:
-        S=[int(power) for power in S]
-        for _2 in range(M):
-            N_new=len(S)
-            S=[S[i]*K for i in range(N_new)]
-            N_new=len(S)
-            for _3 in range(N_new):
-                if S[0]==0:
-                    S.append(S[0])
-                else:
-                    while S[0]>0:
-                        S.append(S[0]%10)
-                        S[0]=S[0]//10
-                S=S[1:]
-        print(len(S))
+        N_W=0
+        for i in range(len(S)):
+            Si=None
+            lst=[int(S[i])]
+            for _2 in range(M+1):
+                Si=[i*K for i in lst]
+                lst=[]
+                for i in Si:
+                    if i==0:
+                        lst.append(i)
+                    else:
+                        while i>0:
+                            lst.append(i%10)
+                            i=i//10
+            N_W+=len(Si)
+        print(N_W)
