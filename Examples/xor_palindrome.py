fbe5085680b382f461759cd3b0c57992cf016f49
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-12 23:02:58 +0530

    main

---
+++ Examples/xor_palindrome.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    N=int(input())
+    S=input()
+    xor_count=[0,0]
+    xor=[[],[]]
+    for i in range(N//2):
+        if S[i]^S[N-i-1]==0:
+            xor_count[0]+=1
+            xor[0]=S[i],S[N-i-1]
+        elif S[i]^S[N-i-1]==1:
+            xor_count[1]+=1
+            xor[1]=S[i],S[N-i-1]
+    xor=max(xor_count[0],xor_count[1])
