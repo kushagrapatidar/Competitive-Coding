f3d7e5e48b89fe1121d1304b9c05c1bc69879bb7
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-12 23:04:09 +0530

    main

--- Examples/xor_palindrome.py
+++ Examples/xor_palindrome.py
@@ -2,12 +2,14 @@ for _ in range(int(input())):
     N=int(input())
     S=input()
     xor_count=[0,0]
-    xor=[[],[]]
+    ele_lst=[[],[]]
     for i in range(N//2):
         if S[i]^S[N-i-1]==0:
             xor_count[0]+=1
-            xor[0]=S[i],S[N-i-1]
+            ele_lst[0]=[S[i],S[N-i-1]]
         elif S[i]^S[N-i-1]==1:
             xor_count[1]+=1
-            xor[1]=S[i],S[N-i-1]
+            ele_lst[1]=[S[i],S[N-i-1]]
     xor=max(xor_count[0],xor_count[1])
+    print(xor)
+    print(xor)
