92cf6d16579a2b49da816bfec0fb05fde340e2f9
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-13 01:11:04 +0530

    main

--- Examples/sequence_gcd.py
+++ Examples/sequence_gcd.py
@@ -1,3 +1,11 @@
+from math import gcd
+def find_gcd(A):
+    GCD=0
+    for i in A:
+        GCD=gcd(GCD,i)
+    return GCD
+
 for _ in range(int(input())):
     N,M=map(int,input().split())
     A=[x for x in map(int,input().split())]
+    print(find_gcd(A))
