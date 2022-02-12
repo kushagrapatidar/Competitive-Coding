d6e097f42e0b65eba6ce8c276e9859847cebbb13
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-12 23:36:13 +0530

    main

--- Examples/xor_palindrome.py
+++ Examples/xor_palindrome.py
@@ -1,15 +1,23 @@
 for _ in range(int(input())):
     N=int(input())
     S=input()
-    xor_count=[0,0]
-    ele_lst=[[],[]]
-    for i in range(N//2):
-        if S[i]^S[N-i-1]==0:
-            xor_count[0]+=1
-            ele_lst[0]=[S[i],S[N-i-1]]
-        elif S[i]^S[N-i-1]==1:
-            xor_count[1]+=1
-            ele_lst[1]=[S[i],S[N-i-1]]
-    xor=max(xor_count[0],xor_count[1])
-    print(xor)
-    print(ele_lst)
+    counts=[0,0]
+    
+    for i in range(N):
+        
+        if S[i]=='0':
+            counts[0]+=1
+
+        elif S[i]=='1':
+            counts[1]+=1
+    
+    if N%2==0:
+        if (max(counts[0],counts[1])-min(counts[0],counts[1]))%2==0 and (counts[0]%2 or counts[1]%2)==0:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        if (max(counts[0],counts[1])-min(counts[0],counts[1]))%2==1:
+            print("Yes")
+        else:
+            print("No")
