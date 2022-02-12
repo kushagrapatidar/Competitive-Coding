7bd049f9eb1d46adbddef2de1188abc95a9667e8
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-12 23:48:56 +0530

    main

--- Examples/xor_palindrome.py
+++ Examples/xor_palindrome.py
@@ -1,18 +1,23 @@
 from tokenize import String
 
 
-N=0
+N=[]
 Strings=[]
 results=[]
+
 for _ in range(int(input())):
     N=int(input())
     Strings.append(input())
-for S in Strings:
-    if len(S)!=N:
-        print("No")
+
+for j in range(len(Strings)):
+    S=String[j]
+
+    if len(S)!=N[i]:
+        results.append("No")
     else:
         counts=[0,0]
-        for i in range(N):
+        
+        for i in range(N[i]):
             
             if S[i]=='0':
                 counts[0]+=1
@@ -20,13 +25,15 @@ for S in Strings:
             elif S[i]=='1':
                 counts[1]+=1
         
-        if N%2==0:
+        if N[i]%2==0:
             if (max(counts[0],counts[1])-min(counts[0],counts[1]))%2==0 and (counts[0]%2 or counts[1]%2)==0:
                 results.append("Yes")
             else:
                 results.append("No")
+        
         else:
             if (max(counts[0],counts[1])-min(counts[0],counts[1]))%2==1:
                 results.append("Yes")
             else:
                 results.append("No")
+for result in results: print(result)
