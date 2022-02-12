b4f70b48c24629de27a858749176f9f571357efa
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-12 19:05:18 +0530

    main

--- Examples/bitwise_swaps.py
+++ Examples/bitwise_swaps.py
@@ -1,16 +1,9 @@
-#Bubble Sort
-
 for _ in range(int(input())):
     N=int(input())
-    arr=map(int,input().spit())
-    arr_sorted=arr.copy().sort()
-    for j in range(len(arr)):
-        for i in range(len(arr)-1):
-            num=arr[i]
-            next_num=arr[i+1]
-            if num>next_num and num&next_num!=0:
-                arr[i],arr[i+1]=next_num,num
-    if arr==arr_sorted:
-        print("Yes")
-    else:
-        print("No")
+    arr=[x for x in map(int,input().split())]
+    lst=[]
+    for _ in range(N):
+        for _2 in range(N):
+            if _!=_2 and arr[_]&arr[_2]!=0 and ([_,_2] not in lst or [_2,_] not in lst):
+                lst.append([_,_2]) 
+    print(lst) 
--- Examples/nofix.py
+++ Examples/nofix.py
@@ -1,24 +1,11 @@
-def check_arr(arr):
-    for _ in range(len(arr)):
-        if arr[_]==_+1:
-            return _
-    return -1
-def count_operations(arr,count):
-    index=check_arr(arr)
-    K=1
-    if index==0: K=len(arr)
-    if index==-1:
-        print(count)
-    else:
-        arr.insert(index,K)
-        count+=1
-        count_operations(arr,count)
-
 for _ in range(int(input())):
     N=int(input())
-    arr=map(int, input().split())
-    arr2=[]
-    for x in arr:
-        arr2.append(x)
-    arr=arr2
-    count_operations(arr,0)
+    A=[]
+    for x in map(int,input().split()):
+        A.append(x)
+    x=0
+    for i in range(N):
+        num=1+i+x
+        if num==A[i]:
+            x+=1
+    print(x)
