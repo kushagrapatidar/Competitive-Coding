87909c9687a05b37bb71e1a4ba4913e0fbebef25
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-14 14:50:56 +0530

    main

--- Examples/bitwise_swaps.py
+++ Examples/bitwise_swaps.py
@@ -2,17 +2,14 @@ for _ in range(int(input())):
     N=int(input())
     arr=[x for x in map(int,input().split())]
     arr_sorted=sorted(arr.copy())
-    if arr==arr_sorted:
+    for i in range(N):
+        j=N-1
+        while j>i:
+            if arr[i]&arr[j]!=0 and arr[i]>arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+                j-=1
+            j-=1
+    if arr!=arr_sorted:
         print("No")
     else:
-        for i in range(N):
-            j=N-1
-            while j>i:
-                if arr[i]&arr[j]!=0 and arr[i]>arr[j]:
-                    arr[i],arr[j]=arr[j],arr[i]
-                    j-=1
-                j-=1
-        if arr!=arr_sorted:
-            print("No")
-        else:
-            print("Yes")
+        print("Yes")
