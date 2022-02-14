ee8ff57e9678da238db52a8f5a959dfdea908120
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-14 13:51:09 +0530

    main

--- Examples/bitwise_swaps.py
+++ Examples/bitwise_swaps.py
@@ -1,14 +1,20 @@
+from time import time
+a=time()
 for _ in range(int(input())):
     N=int(input())
     arr=[x for x in map(int,input().split())]
     arr_sorted=sorted(arr.copy())
-    for i in range(N):
-        j=N-1
-        while j>i:
-            if i!=j and arr[i]&arr[j]!=0 and arr[i]>arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
-            j-=1
-    if arr!=arr_sorted:
-        print("No")
-    else:
+    if arr==arr_sorted:
         print("Yes")
+    else:
+        for i in range(N):
+            j=N-1
+            while j>i:
+                if i!=j and arr[i]&arr[j]!=0 and arr[i]>arr[j]:
+                    arr[i],arr[j]=arr[j],arr[i]
+                j-=1
+        if arr!=arr_sorted:
+            print("No")
+        else:
+            print("Yes")
+print(time()-a)
