76cf344b1ceb704625e320d2ca664cf69fb92a57
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-03-13 20:31:52 +0530

    main

--- Examples/SubarrayXOR.py
+++ Examples/SubarrayXOR.py
@@ -2,16 +2,12 @@ for _ in range(int(input())):
     N=int(input())
     S=input()
     l=len(S)
-    lst=[]
-    i=1
-    while i<=N:
+    xor=0
+    for i in range(1,N+1):
         for j in range(N):
             if j+i>=N:
-                lst.append(int(S[j:N],2))
+                xor^=int(S[j:N],2)
                 break
             else:
-                lst.append(int(S[j:j+i],2))
-        i+=1
-    for i in range(1,len(lst)):
-        lst[i]=lst[i-1]^lst[i]
-    print(lst[-1]%998244353)
+                xor^=int(S[j:j+i],2)
+    print(xor%998244353)
