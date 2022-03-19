c4f493b549cbaa8ccff336a2597ccc5e7e0fe6e6
Kushagra Patidar <triponn.team@gmail.com>
2022-03-19 17:26:41 +0530

    Update CounttheOnes.py

--- Examples/CounttheOnes.py
+++ Examples/CounttheOnes.py
@@ -1,3 +1,8 @@
 for _ in range(int(input())):
     N,K=map(int,input().split())
-    
+    lst=[]
+    for i in range(1,pow(2,N)):
+        lst.append(bin(i).replace("0b",""))
+    for in range(len(lst)):
+        if 
+    print(lst)
